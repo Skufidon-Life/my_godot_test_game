092ace26e0a0c5aad99985c133b2861576dd34a8
Alex <alexandrmeskin@gmail.com>
2025-08-06 22:19:10 +0300

    добавил оружие

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -5,6 +5,10 @@ const SPEED = 2.0
 const JUMP_VELOCITY = 4.5
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 var player
+var is_provoked: bool = false
+var attack_range := 1.5
+var agro_range:= 15.0
+
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -19,11 +23,30 @@ func _physics_process(delta: float) -> void:
 
 	
 	var direction = global_position.direction_to(next_postion)
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	var distance = global_position.distance_to(player.global_position)
+	
+	if distance < agro_range:
+		is_provoked = true
+		
+	if is_provoked:
+		if distance <= attack_range:
+			attack()
+			
+		if direction:
+			look_at_target(direction)
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	move_and_slide()
 
-	#move_and_slide()
+
+func look_at_target(direction):
+	var adjusted_direction = direction
+	adjusted_direction.y = 0 
+	look_at(adjusted_direction + global_position, Vector3.UP, true)
+	
+func attack():
+	print(1)
